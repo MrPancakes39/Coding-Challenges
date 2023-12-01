973d537fa569f95159c2b1831808909e3e6c505f
MrPancakes39 <salmannhassan39@gmail.com>
2023-12-01 22:11:28 +0200

    aoc_2023_day1_p2

--- advent_of_code/2023/day_01/day_01_rs/src/main.rs
+++ advent_of_code/2023/day_01/day_01_rs/src/main.rs
@@ -1,7 +1,9 @@
 
-const INPUT: &'static str = include_str!("./input.txt");
+// const INPUT: &'static str = include_str!("./input.txt");
+const INPUT2: &'static str = include_str!("./input.txt");
 
 fn line_to_digit(line: &str) -> i32 {
+    let line = replace_num_in_line(line);
     let mut iter = line
     .chars()
     .filter(|f| f.is_numeric());
@@ -14,9 +16,35 @@ fn line_to_digit(line: &str) -> i32 {
     }
 }
 
+const LOOKUP_ARRAY: [&str; 9] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+fn get_str_number(num_str: &str) -> i32 {
+    LOOKUP_ARRAY
+    .iter()
+    .position(|&s| s == num_str)
+    .map(|i| (i as i32) + 1)
+    .unwrap()
+}
+
+fn replace_num_in_line(line: &str) -> String {
+    let mut new_line = line.to_string();
+    for num_str in LOOKUP_ARRAY {
+        let mut c = num_str.chars();
+        let first_letter = c.next().unwrap();
+        let last_letter = c.next_back().unwrap();
+        new_line = new_line.replace(
+            num_str,
+            &format!("{}{}{}", first_letter, get_str_number(num_str), last_letter)
+        );
+    }
+    new_line
+}
+
 fn main() {
-    let answer = INPUT.lines().map(line_to_digit).sum::<i32>();
-    println!("Part 1: {:?}.\n", answer);
+    // let answer = INPUT.lines().map(line_to_digit).sum::<i32>();
+    // println!("Part 1: {:?}.\n", answer);
+    let answer = INPUT2.lines().map(line_to_digit).sum::<i32>();
+    println!("Part 2: {:?}.\n", answer);
 }
 
 #[cfg(test)]
