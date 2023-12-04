d2586fab034b0b11ddeffd8c7190f51c0022cf87
MrPancakes39 <salmannhassan39@gmail.com>
2023-12-04 17:27:32 +0200

    aoc_2023_day3_p2_fail

--- advent_of_code/2023/day_03/src/main.rs
+++ advent_of_code/2023/day_03/src/main.rs
@@ -106,18 +106,229 @@ fn get_part_nums_sum(input: &str) -> usize {
     sum
 }
 
+fn slice_to_num(slc: &[char]) -> usize {
+    slc.iter().collect::<String>().parse::<usize>().unwrap()
+}
+
+fn vec_start_num_left(vec: &Vec<char>, i: usize) -> Option<(usize, &char)> {
+    vec.iter()
+        .enumerate()
+        .take(i)
+        .rev()
+        .take_while(|(_, c)| c.is_numeric())
+        .last()
+}
+
+fn vec_end_num_left(vec: &Vec<char>, i: usize) -> Option<(usize, &char)> {
+    vec.iter()
+        .enumerate()
+        .skip(i)
+        .take_while(|(_, c)| c.is_numeric())
+        .last()
+}
+
+fn vec_start_num_right(vec: &Vec<char>, i: usize) -> Option<(usize, &char)> {
+    vec.iter()
+        .enumerate()
+        .skip(i + 1)
+        .rev()
+        .take_while(|(_, c)| c.is_numeric())
+        .last()
+}
+
+fn vec_end_num_right(vec: &Vec<char>, i: usize) -> Option<(usize, &char)> {
+    vec.iter()
+        .enumerate()
+        .skip(i + 1)
+        .take_while(|(_, c)| c.is_numeric())
+        .last()
+}
+
+fn get_gear_part_sum(input: &str) -> usize {
+    let mut sum = 0;
+    let input_lines: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    input_lines.windows(3).for_each(|window| {
+        assert_eq!(window.len(), 3);
+        let prev = &window[0];
+        let current = &window[1];
+        let next = &window[2];
+
+        // println!("prev:\t\t{:?}", prev);
+        // println!("current:\t{:?}", current);
+        // println!("next:\t\t{:?}", next);
+        // println!("-----------------------\n");
+
+        let mut iter = current.iter().enumerate();
+        while let Some((i, &ch)) = iter.next() {
+            if ch != '*' {
+                continue;
+            }
+
+            let is_gear_part = {
+                let mut counter = 0;
+                counter += prev[i].is_numeric() as i32;
+                counter += next[i].is_numeric() as i32;
+                if i != 0 {
+                    // not start of line
+                    counter += prev[i - 1].is_numeric() as i32;
+                    counter += current[i - 1].is_numeric() as i32;
+                    counter += next[i - 1].is_numeric() as i32;
+                }
+                if i != current.len() - 1 {
+                    // not end of line
+                    counter += prev[i + 1].is_numeric() as i32;
+                    counter += current[i + 1].is_numeric() as i32;
+                    counter += next[i + 1].is_numeric() as i32;
+                }
+                counter > 1
+            };
+
+            if is_gear_part {
+                // println!("row_i={row_i} i={i}, ch={ch}, is_gear_part={is_gear_part}.");
+                let mut gear_ratio = 1;
+
+                // check left
+                if i != 0 {
+                    // top left
+                    if prev[i] == '.' && prev[i - 1].is_numeric() {
+                        let start = vec_start_num_left(prev, i).map(|(i, _)| i).unwrap();
+                        let end = i - 1;
+                        let num = slice_to_num(&prev[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                    // middle left
+                    if current[i - 1].is_numeric() {
+                        let start = vec_start_num_left(current, i).map(|(i, _)| i).unwrap();
+                        let end = i - 1;
+                        let num = slice_to_num(&current[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                    // bottom left
+                    if next[i] == '.' && next[i - 1].is_numeric() {
+                        let start = vec_start_num_left(next, i).map(|(i, _)| i).unwrap();
+                        let end = i - 1;
+                        let num = slice_to_num(&next[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                }
+                // check right
+                if i != current.len() - 1 {
+                    // top right
+                    if prev[i] == '.' && prev[i + 1].is_numeric() {
+                        // println!("p = {:?}, i = {i:?}", vec_start_num_right(prev, i));
+                        let start = i + 1;
+                        let end = vec_end_num_right(prev, i).map(|(i, _)| i).unwrap();
+                        // println!("start = {:?}, end = {:?}", start, end);
+                        let num = slice_to_num(&prev[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                    // middle right
+                    if current[i + 1].is_numeric() {
+                        let start = i + 1;
+                        let end = vec_end_num_right(current, i).map(|(i, _)| i).unwrap();
+                        let num = slice_to_num(&current[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                    // bottom right
+                    if next[i] == '.' && next[i + 1].is_numeric() {
+                        // println!("q = {:?}, i = {i:?}", vec_start_num_right(prev, i));
+                        let start = i + 1;
+                        let end = vec_end_num_right(next, i).map(|(i, _)| i).unwrap();
+                        // println!("start={start:?}, end={end:?}");
+                        let num = slice_to_num(&next[start..=end]);
+                        // println!("{:?}", num)
+                        gear_ratio *= num;
+                    }
+                }
+                // check center
+                if prev[i].is_numeric() {
+                    let start = match prev
+                        .iter()
+                        .enumerate()
+                        .take(i)
+                        .rev()
+                        .take_while(|(_, c)| c.is_numeric())
+                        .last()
+                    {
+                        Some((j, _)) => j, // if starts before *
+                        None => i,         // if it starts above *
+                    };
+                    let end = prev
+                        .iter()
+                        .enumerate()
+                        .skip(i)
+                        .take_while(|(_, c)| c.is_numeric())
+                        .last()
+                        .map(|(i, _)| i)
+                        .unwrap();
+                    // dbg!(start);
+                    // dbg!(end);
+                    let num = slice_to_num(&prev[start..=end]);
+                    // println!("{:?}", num)
+                    gear_ratio *= num;
+                }
+                if next[i].is_numeric() {
+                    let start = match next
+                        .iter()
+                        .enumerate()
+                        .take(i)
+                        .rev()
+                        .take_while(|(_, c)| c.is_numeric())
+                        .last()
+                    {
+                        Some((j, _)) => j, // if starts before *
+                        None => i,         // if it starts above *
+                    };
+                    let end = next
+                        .iter()
+                        .enumerate()
+                        .skip(i)
+                        .take_while(|(_, c)| c.is_numeric())
+                        .last()
+                        .map(|(i, _)| i)
+                        .unwrap();
+                    // dbg!(start);
+                    // dbg!(end);
+                    let num = slice_to_num(&next[start..=end]);
+                    // println!("{:?}", num)
+                    gear_ratio *= num;
+                }
+
+                // dbg!(gear_ratio);
+                sum += gear_ratio;
+            }
+            // println!("counter: {counter}");
+        }
+        // println!("-----------------------\n");
+    });
+
+    sum
+}
+
 fn part1() {
     let part1 = get_part_nums_sum(INPUT);
     println!("Part 1: {:?}", part1);
 }
 
+fn part2() {
+    let part2 = get_gear_part_sum(INPUT);
+    println!("Part 2: {:?}", part2);
+}
+
 fn main() {
-    part1();
+    // part1();
+    part2();
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_part_nums_sum;
+    use crate::{get_gear_part_sum, get_part_nums_sum};
 
     #[test]
     fn check_output() {
@@ -133,5 +344,21 @@ mod tests {
 .664.598..";
         let output = get_part_nums_sum(input);
         assert_eq!(output, 4361)
+    }
+
+    #[test]
+    fn check_output_part2() {
+        let input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        let output = get_gear_part_sum(input);
+        assert_eq!(output, 467835)
     }
 }
