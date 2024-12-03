83e8abba6a81c2ef70c2ceee3cfebb1693198f28
MrPancakes39 <salmannhassan39@gmail.com>
2024-12-03 08:49:38 +0200

    Day 2 Part 2

--- advent_of_code/2024/d02/src/main.rs
+++ advent_of_code/2024/d02/src/main.rs
@@ -1,3 +1,5 @@
+const INPUT: &'static str = include_str!("./input.txt");
+
 const EX: &'static str = r#"7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
@@ -39,8 +41,39 @@ fn part1(input: &str) -> i32 {
     })
 }
 
+fn part2(input: &str) -> i32 {
+    input.lines().fold(0, |acc, line| {
+        let line: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse().ok())
+            .collect();
+
+        if is_safe(&line) {
+            return acc + 1;
+        }
+
+        let is_safe = line.iter().enumerate().any(|(i, _)| {
+            let tmp: Vec<i32> = line
+                .iter()
+                .enumerate()
+                .filter_map(|(j, &v)| match j == i {
+                    true => None,
+                    false => Some(v),
+                })
+                .collect();
+
+            is_safe(&tmp)
+        });
+
+        match is_safe {
+            true => acc + 1,
+            false => acc,
+        }
+    })
+}
+
 fn main() {
-    let answer = part1(EX);
+    let answer = part2(&INPUT);
     println!("Answer: {answer}");
 }
 
@@ -51,5 +84,10 @@ mod test {
     #[test]
     fn test_part1() {
         assert_eq!(2, part1(EX));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(4, part2(EX));
     }
 }
